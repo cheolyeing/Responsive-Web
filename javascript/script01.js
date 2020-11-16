dd9e5081e3abb1db56b09f60951776c97fbb1b9c
cheolyeing <cheolyeing@gmail.com>
2020-11-17 01:50:28 +0900

    Include jquery

--- javascript/script01.js
+++ javascript/script01.js
@@ -1,5 +1,5 @@
 var script = document.createElement('script');
-script.src = '../javascript/jquery.js';
+script.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
 script.type = 'text/javascript';
 document.getElementsByTagName('head')[0].appendChild(script);
 
