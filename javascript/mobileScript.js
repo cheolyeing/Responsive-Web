9dcd81dbd1c4cc6de4357e1990476d7d7c7d437e
cheolyeing <cheolyeing@gmail.com>
2020-12-14 04:31:03 +0900

    Adapt Final Mobile mode

--- javascript/mobileScript.js
+++ javascript/mobileScript.js
@@ -2,18 +2,8 @@ const WIDTH = window.innerWidth;
 const HEIGHT = window.innerHeight;
 
 window.onload = function () {
-
-    console.log(WIDTH)
-
-    {
-        var html = document.html
-        var body = document.body.style
-
-        console.log(html)
-        console.log(body)
-        body.margin = 0;
-        body.padding = 0;
-    }
+    loadingMotion();
+    document.body.style.overflowY = 'hidden';
 
     {
         var video = document.getElementById('main_video').style
@@ -32,9 +22,94 @@ window.onload = function () {
         main.backgroundImage = 'url(./src/mobile.png)';
         main.width = WIDTH + 'px';
         main.height = height * WIDTH / width + 'px';
-        main.backgroundSize = 'contain';
-        console.log(main.width, main.height)
     }
 
-    console.log("OK")
+    {
+        var loadingBack = document.getElementById('loading_back').style
+
+        loadingBack.position = 'fixed';
+        loadingBack.width = WIDTH + 'px';
+        loadingBack.height = HEIGHT + 'px';
+        loadingBack.backgroundColor = 'black';
+        loadingBack.zIndex = 2;
+        loadingBack.opacity = 0.8;
+    }
+
+    {
+        const height = 1080 * WIDTH / 1920;
+        const top = (HEIGHT - height) / 2;
+
+        var loadingFront = document.getElementById('loading_front').style;
+
+        loadingFront.position = 'fixed';
+        loadingFront.width = WIDTH * 0.9 + 'px';
+        loadingFront.left = WIDTH * 0.05 + 'px';
+        loadingFront.height = height + 'px';
+        loadingFront.backgroundColor = 'black';
+        loadingFront.top = top + 'px';
+        loadingFront.boxShadow = '0px 0px 10px grey';
+        loadingFront.zIndex = 3;
+    }
+
+    {
+        const height = 1080 * WIDTH / 1920;
+        const gap = (HEIGHT - height) / 2;
+
+        var loadingFace1 = document.getElementById('loading_face1').style;
+        loadingFace1.position = 'fixed';
+        loadingFace1.width = WIDTH * 0.034 + 'px';
+        loadingFace1.height = WIDTH * 0.034 + 'px';
+        loadingFace1.left = WIDTH * (0.5 - 0.017 * 4) + 'px';
+        loadingFace1.top = gap + height * 0.31 + 'px';
+        loadingFace1.backgroundImage = 'url(./src/loading_face1.png)'
+        loadingFace1.zIndex = 4;
+
+        var loadingFace2 = document.getElementById('loading_face2').style;
+        loadingFace2.position = 'fixed';
+        loadingFace2.width = WIDTH * 0.034 + 'px';
+        loadingFace2.height = WIDTH * 0.034 + 'px';
+        loadingFace2.left = WIDTH * (0.5 - 0.017) + 'px';
+        loadingFace2.top = gap + height * 0.31 + 'px';
+        loadingFace2.backgroundImage = 'url(./src/loading_face2.png)'
+        loadingFace2.zIndex = 4;
+
+        var loadingFace3 = document.getElementById('loading_face3').style;
+        loadingFace3.position = 'fixed';
+        loadingFace3.width = WIDTH * 0.034 + 'px';
+        loadingFace3.height = WIDTH * 0.034 + 'px';
+        loadingFace3.right = WIDTH * (0.5 - 0.017 * 4) + 'px';
+        loadingFace3.top = gap + height * 0.31 + 'px';
+        loadingFace3.backgroundImage = 'url(./src/loading_face3.png)'
+        loadingFace3.zIndex = 4;
+
+        var loadingMobile = document.getElementById('loading_mobile').style;
+        loadingMobile.position = 'fixed';
+        loadingMobile.width = WIDTH * 0.49 + 'px';
+        loadingMobile.height = WIDTH * 0.14 + 'px';
+        loadingMobile.left = WIDTH * 0.255 + 'px';
+        loadingMobile.top = gap + height * 0.426 + 'px';
+        loadingMobile.backgroundImage = 'url(./src/loading_mobile.png)'
+        loadingMobile.zIndex = 4;
+    }
+
+    setTimeout(() => {
+        document.getElementById('loading_back').style.display = 'none';
+        document.getElementById('loading_front').style.display = 'none';
+        document.body.style.overflowY = 'auto';
+    }, 5000);
+}
+
+function loadingMotion() {
+    var index = 0;
+    setInterval(() => {
+        index++;
+        for (var i = 1; i < 4; i++) {
+            var loadingFace = document.getElementById('loading_face' + i).style;
+            loadingFace.opacity = 0.2;
+        }
+        var target = index % 3 + 1;
+        var loadingFace = document.getElementById('loading_face' + target).style;
+        loadingFace.opacity = 1;
+        if (index > 20) return;
+    }, 300)
 }
