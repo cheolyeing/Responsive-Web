30b073ccb58b0732c81152b2fe4ece3f945a6c91
cheolyeing <cheolyeing@gmail.com>
2020-12-13 23:56:11 +0900

    Control Mobile size with Video

--- javascript/mobileScript.js
+++ javascript/mobileScript.js
@@ -3,29 +3,38 @@ const HEIGHT = window.innerHeight;
 
 window.onload = function () {
 
-    {
-        var videoBox = document.getElementById("video_box").style
-        const width = 1920;
-        const height = 1080;
+    console.log(WIDTH)
 
-        videoBox.backgroundColor = 'reda';
-        videoBox.width = WIDTH;
-        videoBox.height = height * WIDTH / width;
+    {
+        var html = document.html
+        var body = document.body.style
+
+        console.log(html)
+        console.log(body)
+        body.margin = 0;
+        body.padding = 0;
     }
 
     {
-        var viedo = document.getElementById('main_video').style
+        var video = document.getElementById('main_video').style
         const width = 1920;
         const height = 1080;
+        video.width = WIDTH + 'px';
+        video.height = height * WIDTH / width + 'px';
+        video.backgroundSize = 'contain';
     }
 
     {
-        var main = document.getElementById("main").style
+        var main = document.getElementById("main_box").style
         const width = 2037;
         const height = 32768;
 
-        main.width = WIDTH;
-        main.height = height * WIDTH / width;
+        main.backgroundImage = 'url(./src/mobile.png)';
+        main.width = WIDTH + 'px';
+        main.height = height * WIDTH / width + 'px';
         main.backgroundSize = 'contain';
+        console.log(main.width, main.height)
     }
+
+    console.log("OK")
 }
